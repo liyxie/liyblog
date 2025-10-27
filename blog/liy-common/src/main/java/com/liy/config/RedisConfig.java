fbc83034bc0e9d6af59e1cb9a5f6979ff2501807
liyxie <liy200110@163.com>
2025-10-27 11:40:23 +0800

    redis启动警告

--- blog/liy-common/src/main/java/com/liy/config/RedisConfig.java
+++ blog/liy-common/src/main/java/com/liy/config/RedisConfig.java
@@ -1,88 +1,45 @@
 package com.liy.config;
 
-import io.lettuce.core.ClientOptions;
-import io.lettuce.core.SocketOptions;
-import io.lettuce.core.TimeoutOptions;
-import io.lettuce.core.cluster.ClusterClientOptions;
-import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-
 /**
- * Redis配置类
- * 用于优化Redis连接性能和稳定性
+ * @author blue
+ * @description:
+ * @date 2021/7/30 15:16
  */
 @Configuration
 public class RedisConfig {
 
     /**
-     * 配置Lettuce客户端
+     *  配置redis序列化
+     * @author blue
+     * @date: 2021/7/30 15:17
      */
     @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory() {
-        // 配置Socket选项
-        SocketOptions socketOptions = SocketOptions.builder()
-                .connectTimeout(Duration.ofSeconds(10))
-                .keepAlive(true)
-                .tcpNoDelay(true)
-                .build();
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
 
-        // 配置超时选项
-        TimeoutOptions timeoutOptions = TimeoutOptions.builder()
-                .timeoutCommands()
-                .fixedTimeout(Duration.ofSeconds(10))
-                .build();
-
-        // 配置客户端选项
-        ClientOptions clientOptions = ClientOptions.builder()
-                .socketOptions(socketOptions)
-                .timeoutOptions(timeoutOptions)
-                .autoReconnect(true)
-                .suspendReconnectOnProtocolFailure(true)
-                .cancelCommandsOnReconnectFailure(true)
-                .build();
-
-        // 配置Lettuce客户端
-        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .clientOptions(clientOptions)
-                .commandTimeout(Duration.ofSeconds(10))
-                .shutdownTimeout(Duration.ofMillis(100))
-                .useSsl(false)
-                .build();
-
-        return new LettuceConnectionFactory(clientConfig);
-    }
-
-    /**
-     * 配置RedisTemplate
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
 
         // 使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Hash的key也采用StringRedisSerializer的序列化方式
         template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
 
-        // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        // 开启事务
-        template.setEnableTransactionSupport(true);
-
-        // 初始化RedisTemplate
         template.afterPropertiesSet();
-
         return template;
     }
 }
